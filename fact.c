b281ed8242a65d1af5f78e6fc895f229778d4f3e
Bavanidevi <bavab.tech@gmail.com>
2017-06-23 12:42:44 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int n,x,sum,f;
+printf("Enter the Number");
+scanf("%d",&n);
+printf("Factorial of %d is %d",n,rec(n));
+getch();
+}
+int rec(x)
+{
+if(x==1)
+  return(1);
+  else
+  f=x*rec(x-1);
+ return(f);
+  }
