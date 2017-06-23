1a430821e203678aa447503718e4282bfef78529
Bavanidevi <bavab.tech@gmail.com>
2017-06-23 09:05:16 +0530

    Create sumfor.c

---
+++ sumfor.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int n,i,sum;
+printf("Enter the Number");
+scanf("%d",&n);
+for(i=1i<=n;i++)
+{
+sum=sum+n;
+}
+printf("The sum is %d",sum);
+getch();
+}
