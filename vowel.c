2363ec903bda0914aee6ff4c19dab10bf2a37f4e
Bavanidevi <bavab.tech@gmail.com>
2017-06-27 16:46:01 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+char a1;
+printf("Enter the Number");
+scanf("%c",&a);
+if(a1=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+printf("Entered character %c is vowel",a1);
+}
+else
+{
+printf("Entered character %c is consonant",a1);
+}
+getch();
+}
