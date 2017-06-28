9769595a190d21c103d2971685b211569072cc21
Bavanidevi <bavab.tech@gmail.com>
2017-06-28 12:31:29 +0530

    Create sum1.c

---
+++ sum1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int l,i=1,sumn,n=1;
+printf("Enter the Limit");
+scanf("%d",&l);
+while(i<=l)
+{
+sum=sum+n;
+i++;
+}
+printf("The sum is %d",sum);
+getch();
+}
